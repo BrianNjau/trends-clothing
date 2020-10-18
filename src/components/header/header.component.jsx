ab9f5bcc6f31330978df1872be08657beaf6b18c
Brian Njau <brian.njau@strathmore.edu>
2020-10-18 15:58:35 +0300

    Added redux and implemented userReducer and  userActions to header and app components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,35 +1,38 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-import'./header.styles.scss';
+import "./header.styles.scss";
 
-import {auth} from '../../firebase/firebase.utils';
+import { auth } from "../../firebase/firebase.utils";
 
-import {ReactComponent as Logo}from '../../assets/crwn.svg';
+import { ReactComponent as Logo } from "../../assets/crwn.svg";
 
-const Header = ({currentUser}) => (
-    <div className = 'header'>
-        <Link className='logo-container' to= "/">
-        <Logo  className='logo'/>
-        </Link>
-        <div className='options'>
-        <Link className='option' to='/shop'>
+const Header = ({ currentUser }) => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
+    <div className="options">
+      <Link className="option" to="/shop">
         SHOP
-        </Link>
-       <Link className='option' to='/shop'>
+      </Link>
+      <Link className="option" to="/shop">
         CONTACT
-        </Link>
-    {
-        currentUser ?
-        <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-        :
-        <Link className='option' to='/signin' >SIGN IN</Link>
-
-    }
-
+      </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
         </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
+      )}
     </div>
-
-
-)
-export default Header;
+  </div>
+);
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
+export default connect(mapStateToProps)(Header);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,51 +1,49 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-    apiKey: "AIzaSyA_NI7Vwli4AyBQ-4L2axzjmUACqDDntmE",
-    authDomain: "trend-db-b9df1.firebaseapp.com",
-    databaseURL: "https://trend-db-b9df1.firebaseio.com",
-    projectId: "trend-db-b9df1",
-    storageBucket: "trend-db-b9df1.appspot.com",
-    messagingSenderId: "851391375328",
-    appId: "1:851391375328:web:0d9c61d8fd0b1f7f2c6d85"
-  };
+  apiKey: "AIzaSyA_NI7Vwli4AyBQ-4L2axzjmUACqDDntmE",
+  authDomain: "trend-db-b9df1.firebaseapp.com",
+  databaseURL: "https://trend-db-b9df1.firebaseio.com",
+  projectId: "trend-db-b9df1",
+  storageBucket: "trend-db-b9df1.appspot.com",
+  messagingSenderId: "851391375328",
+  appId: "1:851391375328:web:0d9c61d8fd0b1f7f2c6d85",
+};
 
-  export const createUserProfileDocument = async (userAuth, additionalData) =>{
-    if(!userAuth) return ;
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
 
-    const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-    const snapShot = await userRef.get();
+  const snapShot = await userRef.get();
 
-    if(!snapShot.exists){
-      const{displayName, email } = userAuth;
-      const createdAt = new Date();
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
 
-      try{
-        await userRef.set({
-          displayName,
-          email,
-          createdAt,
-          ...additionalData
-        })
-      }catch(error){
-        console.log('error creating user', error.message);
-      }
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
     }
-    return userRef;
-
-
   }
+  return userRef;
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({prompt:'select_account'});
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
-  export default firebase;
+export default firebase;
